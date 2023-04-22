01f3232e0f2cbbcacffe25e0dfe5a7b5cae884fd
Nguyễn Thành Tâm <nguyenthanhtam12b3@gmail.com>
2023-04-22 16:34:16 +0700

    add splash screen

--- lib/src/config/app_router.dart
+++ lib/src/config/app_router.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 
 import '../screens/home_screen.dart';
 import '../screens/not_found_screen.dart';
+import '../screens/splash_screen.dart';
 import '../shared/constants/app_constants.dart';
 
 class AppRouter {
@@ -9,6 +10,7 @@ class AppRouter {
     return CupertinoPageRoute(builder: (context) {
       switch (settings.name) {
         case RouteNames.initial:
+          return const SplashScreen();
         case RouteNames.home:
           return const HomeScreen();
         default:
---
+++ lib/src/screens/splash_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../shared/constants/app_assets.dart';
+
+import '../shared/constants/app_constants.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 2)).whenComplete(
+        () => Navigator.pushReplacementNamed(context, RouteNames.home));
+
+    return Scaffold(
+      body: Center(
+        child: SvgPicture.asset(
+          AppAssets.appLogo,
+          height: 150,
+        ),
+      ),
+    );
+  }
+}
