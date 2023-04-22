34555c2d94a25cab4ea305c218f98f8dbff2686b
Nguyễn Thành Tâm <nguyenthanhtam12b3@gmail.com>
2023-04-22 13:41:17 +0700

    initial structure folders

---
+++ lib/src/story_book_app.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class StoryBookApp extends StatelessWidget {
+  const StoryBookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
